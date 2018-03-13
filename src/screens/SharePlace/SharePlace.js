46d9ce24007d3317a5f94d7ffbaf94091b01b686
Hugo López Mandujano <hugol.soul@gmail.com>
2018-03-13 14:57:27 -0600

    Updating redux with react native

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+
+import PlaceInput from '../../components/PlaceInput/PlaceInput';
+import { addPlace } from '../../store/actions/index';
 
 class SharePlaceScreen extends Component {
+
+  placesAddedHandler = placeName => {
+    this.props.onAddPlace(placeName);
+  }
+
   render () {
     return(
       <View>
-        <Text>Sigue trabajando .....</Text>
+        <PlaceInput onPlaceAdded={this.placesAddedHandler} />
       </View>
     );
   }
 }
 
-export default SharePlaceScreen;
+mapDispatchToProps = dispatch => {
+  return{
+    onAddPlace: (placeName) => dispatch(addPlace(placeName))
+  };
+};
+
+export default connect(null, mapDispatchToProps)(SharePlaceScreen);
