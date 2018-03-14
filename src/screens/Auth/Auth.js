daef6a55e4167e0851f4ef35b67e1336a6280b0f
Hugo López Mandujano <hugol.soul@gmail.com>
2018-03-13 22:49:18 -0600

    Styling Auth Screen

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button  } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 import startMainTabs from '../MainTabs/startMainTabs';
 
 class AuthScreen extends Component {
@@ -10,12 +10,32 @@ class AuthScreen extends Component {
 
   render () {
     return(
-      <View>
-        <Text>Welcome Back!</Text>
+      <View style={styles.container}>
+        <Text>Please Log In!</Text>
+        <Button title="Switch to Login" />
+        <View style={styles.inputContainer}>
+          <TextInput placeholder="Username" style={styles.input} />
+          <TextInput placeholder="Password" style={styles.input} />
+          <TextInput placeholder="Confirm Password" style={styles.input} />
+        </View>
         <Button title="Login" onPress={this.loginHandler} />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  inputContainer: {
+    width: '80%'
+  },
+  input: {
+    width: '100%'
+  }
+});
 
 export default AuthScreen;
