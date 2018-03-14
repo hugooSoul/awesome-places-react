cc6c4b7a3103af6f8e4418c00ac8d3355495a896
Hugo López Mandujano <hugol.soul@gmail.com>
2018-03-14 12:26:27 -0600

    adding ImageBackground in the first navigation and styling it

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet, ImageBackground } from 'react-native';
 import startMainTabs from '../MainTabs/startMainTabs';
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
 import HeadingText from '../../components/UI/HeadingText/HeadingText';
+import MainText from '../../components/UI/MainText/MainText';
+import backgroundImage from '../../assets/beach.jpg';
 
 class AuthScreen extends Component {
 
@@ -12,16 +14,20 @@ class AuthScreen extends Component {
 
   render () {
     return(
-      <View style={styles.container}>
-        <HeadingText>Please Log In</HeadingText>
-        <Button title="Switch to Login" />
-        <View style={styles.inputContainer}>
-          <DefaultInput placeholder="Username" style={styles.input} />
-          <DefaultInput placeholder="Password" style={styles.input} />
-          <DefaultInput placeholder="Confirm Password" style={styles.input} />
+      <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+        <View style={styles.container}>
+            <MainText>
+              <HeadingText>Please Log In</HeadingText>
+            </MainText>
+            <Button title="Switch to Login" />
+            <View style={styles.inputContainer}>
+              <DefaultInput placeholder="Username" style={styles.input} />
+              <DefaultInput placeholder="Password" style={styles.input} />
+              <DefaultInput placeholder="Confirm Password" style={styles.input} />
+            </View>
+            <Button title="Login" onPress={this.loginHandler} />
         </View>
-        <Button title="Login" onPress={this.loginHandler} />
-      </View>
+      </ImageBackground>
     );
   }
 }
@@ -38,6 +44,10 @@ const styles = StyleSheet.create({
   input: {
     backgroundColor: '#eee',
     borderColor: '#bbb'
+  },
+  backgroundImage: {
+    width: '100%',
+    flex: 1
   }
 });
 
